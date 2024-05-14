de520e3b9585414168f3fff5cc4c7346ebc1fef0
SadiaLiana10 <159700782+SadiaLiana10@users.noreply.github.com>
2024-05-14 22:13:26 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let bar = document.querySelector(".bar");
+let sideBar = document.querySelector("#sideBar");
+let cross = document.querySelector(".cross");
+
+bar.addEventListener(`click`, function(){
+    sideBar.classList.add(`active`);
+    // alert(`ok`);
+});
+
+cross.addEventListener(`click` , function(){
+    sideBar.classList.remove(`active`);
+});
+
+sideBar.addEventListener(`click`, function(e){
+    if(e.target.classList.contains(`active`) == true){
+        sideBar.classList.remove(`active`);
+    }
+
+});
+
+
